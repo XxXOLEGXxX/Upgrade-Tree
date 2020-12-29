2ba0d4a500890717892a25ef6ca8c97c350cc02e
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-12-29 14:02:45 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -39,7 +39,10 @@ addLayer("u", {
                   else return false	},
 
     gainMult() {                            // Returns your multiplier to your gain of the prestige resource.
-        return new Decimal(1)
+        let mult = new Decimal(2)
+		if(player.b.magnifyingLevel.gt(0)) mult = mult.div(player.b.magnifyingLevel.add(1))
+		if(hasUpgrade("b", 35)) mult = mult.pow(1.006)
+		return mult
     },
     gainExp() {                             // Returns your exponent to your gain of the prestige resource.
         return new Decimal(1)
@@ -96,9 +99,9 @@ addLayer("w", {
                                             // Also the amount required to unlock the layer.
     type: "static",                         // Determines the formula used for calculating prestige currency.
 	base: 3,
-    exponent() { if(hasUpgrade("u2", 15)) return new Decimal(3).sub(layers.u2.effect())
+    exponent() { if(hasUpgrade("u2", 14)) return new Decimal(3).sub(layers.u2.effect())
 	             else return new Decimal(3) },                          // "normal" prestige gain is (currency^exponent).
-	canBuyMax() { if(hasUpgrade("u2", 15)) return true
+	canBuyMax() { if(hasUpgrade("u2", 14)) return true
                   else return false	},
 
     gainMult() {                            // Returns your multiplier to your gain of the prestige resource.
@@ -132,7 +135,7 @@ addLayer("w", {
 		},
 		41: {
 			title: "c",
-			description: "you gain 10x more prestige points and wrong upgrades affects right upgrades cost",
+			description: "wrong upgrades affects right upgrades cost",
 			cost: new Decimal(3),
 			effect() { return player.w.points.add(1).pow(player.w.points.add(1)) },
 			effectDisplay() { return "/" + format(this.effect()) },
@@ -153,7 +156,8 @@ addLayer("w", {
     doReset(resettingLayer){ // Triggers when this layer is being reset, along with the layer doing the resetting. Not triggered by lower layers resetting, but is by layers on the same row.
         if(layers[resettingLayer].row > this.row) {
 			layerDataReset(this.layer)
-			if(hasUpgrade("u2", 14)) player.w.upgrades.push("11")
+			if(hasUpgrade("u2", 15)) player.w.upgrades.push("11")
+			if(hasUpgrade("b", 25)) player.points = new Decimal(1)
 		}
 	}
 })
@@ -181,7 +185,7 @@ addLayer("r", {
     type: "static",                         // Determines the formula used for calculating prestige currency.
 	base: 3,
     exponent: 3,                          // "normal" prestige gain is (currency^exponent).
-	canBuyMax() { if(hasUpgrade("u2", 15) || hasUpgrade("l", 13)) return true
+	canBuyMax() { if(hasUpgrade("u2", 14) || hasUpgrade("l", 13)) return true
                   else return false	},
 
     gainMult() {                            // Returns your multiplier to your gain of the prestige resource.
@@ -220,7 +224,6 @@ addLayer("r", {
 			description: "Bought upgrades's effect is exponented by the amount of right upgrade's upgrades you have.",
 			effect() { let eff = new Decimal(player.r.upgrades.length).add(1)
                        if(hasUpgrade("r", 22)) eff = eff.mul(upgradeEffect("r", 22))
-					   if(hasUpgrade("l", 13)) eff = eff.pow(eff.root(1.9))
                        return eff },
 			effectDisplay() { return "^" + format(this.effect()) },
 			cost: new Decimal(3)
@@ -241,7 +244,7 @@ addLayer("r", {
     doReset(resettingLayer){ // Triggers when this layer is being reset, along with the layer doing the resetting. Not triggered by lower layers resetting, but is by layers on the same row.
         if(layers[resettingLayer].row > this.row) {
 			layerDataReset(this.layer)
-			if(hasUpgrade("u2", 14)) player.r.upgrades.push("11")
+			if(hasUpgrade("u2", 15)) player.r.upgrades.push("11")
 		}
 	}
 })
@@ -251,6 +254,8 @@ addLayer("b", {
         unlocked: false,                     // You can add more variables here to add them to your layer.
         points: new Decimal(0),             // "points" is the internal name for the main resource of the layer.
 		unlockOrder: new Decimal(0),
+		magnifyingLevel: new Decimal(0),
+		divisionPower: new Decimal(0),
     }},
 
     color: "#6CD748",                       // The color for this layer, which affects many elements.
@@ -262,9 +267,21 @@ addLayer("b", {
     baseResource: "wrong upgrades",                 // The name of the resource your prestige gain is based on.
     baseAmount() { return player.w.points },  // A function to return the current amount of baseResource.
 
-    requires() { let cost = new Decimal(4).pow((player.l.unlocked&&player.u2.unlocked&&!player.b.unlocked)?81:(player.l.unlocked&&!player.b.unlocked || player.u2.unlocked&&!player.b.unlocked)?9:1)
+    requires() { let cost = new Decimal(4).pow((player.l.unlocked&&player.u2.unlocked)?81:(player.l.unlocked || player.u2.unlocked)?3.9:1)
 				 return cost },             // The amount of the base needed to  gain 1 of the prestige currency.
                                             // Also the amount required to unlock the layer.
+	midsection: [
+		["display-text", function() { return hasUpgrade("b", 31) || player.b.magnifyingLevel.gt(0) ? "You have 1 stolen Magnifying Glass and fake Magnifying Levels go BRRRRR-<br/>You have " + format(player.b.magnifyingLevel) + " fake Magnifying Levels, \"multiplying\" base bought upgrade gain by x" + format(player.b.magnifyingLevel.add(1)) + ", which are producing the levels at a rate of 0.002 a second" : ""}],
+		"blank",
+		["display-text", function() { return hasUpgrade("b", 33) || player.b.divisionPower.gt(0) ? "You have 1 stolen Divider, which generates 0.01 fake division power/sec<br/>You have " + format(player.b.divisionPower) + " fake division power, which divides the costs of bought and wrong upgrades by " + format(new Decimal(player.b.divisionPower.add(1)).log10().add(1).pow(0.6)) : ""}],
+		"blank"
+	],
+	
+	update(diff) {
+		if(hasUpgrade("b", 31)) player.b.magnifyingLevel = player.b.magnifyingLevel.add(new Decimal(0.002).mul(diff))
+		if(hasUpgrade("b", 33)) player.b.divisionPower = player.b.divisionPower.add(new Decimal(0.01).mul(diff))
+	},
+	
     type: "static",                         // Determines the formula used for calculating prestige currency.
 	base: 4,
     exponent: 4,                          // "normal" prestige gain is (currency^exponent).
@@ -281,27 +298,113 @@ addLayer("b", {
 	branches: ["w"],
 	
 	upgrades: {
-		rows: 2,
-		cols: 3,
+		rows: 10,
+		cols: 5,
 		11: {
-			title: "test",
-			description: "test",
-			cost: new Decimal(2)
+			title: "Use ducdat0507's TPTM.",
+			description: "Unlocks a bunch of upgrades. Choosing one upgrade in each row will increase every upgrade's price.",
+			cost: new Decimal(1),
+			onPurchase() { player.b.points = new Decimal(1) }
 		},
 		12: {
-			title: "test",
-			description: "test",
-			cost: new Decimal(2)
-		},
-		13: {
-			title: "test",
-			description: "test",
-			cost: new Decimal(2)
+			title: "funny button.",
+			description: "Resets all bought upgrades below.<br/>(Note: You won't get boring upgrades back.)",
+			cost: new Decimal(0),
+			unlocked() { return hasUpgrade("b", 11) },
+			onPurchase() { player.b.upgrades = [11] }
 		},
 		21: {
-			title: "test",
-			description: "test",
-			cost: new Decimal(2)
+			title: "The Modding Tree: Lollipop's Terror",
+			description: "Boosts your prestige point gain based on boring upgrades.",
+			effect() { return new Decimal(player.b.upgrades.length).sub(1).pow(0.2) },
+			effectDisplay() { return "+" + format(this.effect()) },
+			cost() { return new Decimal(1).add(player.b.upgrades.length).sub(1) },
+			unlocked() { return hasUpgrade("b", 11) },
+		},
+		22: {
+			title: "TreeQuest: ???",
+			description: "idk what to add here",
+			cost() { return new Decimal(1).add(player.b.upgrades.length).sub(1) },
+			unlocked() { return hasUpgrade("b", 11) },
+		},
+		23: {
+			title: "The Prestige Web: Links's...",
+			description: "... Uhhhh- you have this much links based on boring upgrades.",
+			effectDisplay() { return formatWhole(new Decimal(player.b.upgrades.length).sub(1)) },
+			cost() { return new Decimal(1).add(player.b.upgrades.length).sub(1) },
+			unlocked() { return hasUpgrade("b", 11) },
+		},
+		24: {
+			title: "The Basic Tree: Basic Combo",
+			description: "You gain 1 more prestige point per second, then the gain is multiplied by 1.5x and then exponented by ^1.05",
+			cost() { return new Decimal(1).add(player.b.upgrades.length).sub(1) },
+			unlocked() { return hasUpgrade("b", 11) },
+		},
+		25: {
+			title: "The Modding Tree: Kickstarter",
+			description: "You start off with 1 prestige point on 2nd and 3rd reset.",
+			cost() { return new Decimal(1).add(player.b.upgrades.length).sub(1) },
+			unlocked() { return hasUpgrade("b", 11) },
+		},
+		31: {
+			title: "The Tree Prestige: Magnifying Glass",
+			description: "You start generating magnifying levels really slowly.",
+			cost() { return new Decimal(1).add(player.b.upgrades.length).sub(2) },
+			unlocked() { return hasUpgrade("b", 21) || hasUpgrade("b", 22) || hasUpgrade("b", 23) || hasUpgrade("b", 24) || hasUpgrade("b", 25) },
+		},
+		32: {
+			title: "The Incrementreeverse: Antimatter",
+			description: "idk what to add here",
+			cost() { return new Decimal(1).add(player.b.upgrades.length).sub(2) },
+			unlocked() { return hasUpgrade("b", 21) || hasUpgrade("b", 22) || hasUpgrade("b", 23) || hasUpgrade("b", 24) || hasUpgrade("b", 25) },
+		},
+		33: {
+			title: "The Unit Tree: Divider",
+			description: "You start generating division power really slowly.",
+			cost() { return new Decimal(1).add(player.b.upgrades.length).sub(2) },
+			unlocked() { return hasUpgrade("b", 21) || hasUpgrade("b", 22) || hasUpgrade("b", 23) || hasUpgrade("b", 24) || hasUpgrade("b", 25) },
+		},
+		34: {
+			title: "The Minecraft Tree: Wood",
+			description: "Prestige point gain is multiplied based on blocks... Oh right, you don't have them.",
+			cost() { return new Decimal(1).add(player.b.upgrades.length).sub(2) },
+			unlocked() { return hasUpgrade("b", 21) || hasUpgrade("b", 22) || hasUpgrade("b", 23) || hasUpgrade("b", 24) || hasUpgrade("b", 25) },
+		},
+		35: {
+			title: "The Bitcoin Tree: Overglocked",
+			description: "Multiplies prestige point gain by x1.091 and exponents bought upgrade gain by ^1.006.",
+			cost() { return new Decimal(1).add(player.b.upgrades.length).sub(2) },
+			unlocked() { return hasUpgrade("b", 21) || hasUpgrade("b", 22) || hasUpgrade("b", 23) || hasUpgrade("b", 24) || hasUpgrade("b", 25) },
+		},
+		41: {
+			title: "The Leveling Tree: Rubies",
+			description: "Lamp oil, rope, bombs! You want it? It's yours, my friend, as long as you have enough rubies!",
+			cost() { return new Decimal(1).add(player.b.upgrades.length).sub(3) },
+			unlocked() { return hasUpgrade("b", 31) || hasUpgrade("b", 32) || hasUpgrade("b", 33) || hasUpgrade("b", 34) || hasUpgrade("b", 35) },
+		},
+		42: {
+			title: "Prestige Tree Rewritten: Achievement",
+			description: "Unlocks an achievement feature.",
+			cost() { return new Decimal(1).add(player.b.upgrades.length).sub(3) },
+			unlocked() { return hasUpgrade("b", 31) || hasUpgrade("b", 32) || hasUpgrade("b", 33) || hasUpgrade("b", 34) || hasUpgrade("b", 35) },
+		},
+		43: {
+			title: "The Tree of Insanity: Braincell",
+			description: "By slowly realizing what's going on with 3rd row... You start losing braincells.",
+			cost() { return new Decimal(1).add(player.b.upgrades.length).sub(3) },
+			unlocked() { return hasUpgrade("b", 31) || hasUpgrade("b", 32) || hasUpgrade("b", 33) || hasUpgrade("b", 34) || hasUpgrade("b", 35) },
+		},
+		44: {
+			title: " The Solar System Tree: Mercuric Points",
+			description: "Gain a multiplier to prestige points based on dumped prestige points.<br/>(log10(dumped))",
+			cost() { return new Decimal(1).add(player.b.upgrades.length).sub(3) },
+			unlocked() { return hasUpgrade("b", 31) || hasUpgrade("b", 32) || hasUpgrade("b", 33) || hasUpgrade("b", 34) || hasUpgrade("b", 35) },
+		},
+		45: {
+			title: "All undeveloped TMT in a nutshell: Literally nothing.",
+			description: "There's only points and prestige points. That's it.",
+			cost() { return new Decimal(1).add(player.b.upgrades.length).sub(3) },
+			unlocked() { return hasUpgrade("b", 31) || hasUpgrade("b", 32) || hasUpgrade("b", 33) || hasUpgrade("b", 34) || hasUpgrade("b", 35) },
 		}
 	}
 })
@@ -322,7 +425,7 @@ addLayer("l", {
     baseResource: "bought upgrades",                 // The name of the resource your prestige gain is based on.
     baseAmount() { return player.u.points },  // A function to return the current amount of baseResource.
 
-    requires() { let cost = new Decimal(10).pow((player.b.unlocked&&player.u2.unlocked&&!player.l.unlocked)?81:(player.b.unlocked&&!player.l.unlocked || player.u2.unlocked&&!player.l.unlocked)?9:1)
+    requires() { let cost = new Decimal(10).pow((player.u2.unlocked&&!player.l.unlocked)?2.1:1)
 				 return cost },             // The amount of the base needed to  gain 1 of the prestige currency.
                                             // Also the amount required to unlock the layer.
     type: "static",                         // Determines the formula used for calculating prestige currency.
@@ -408,9 +511,9 @@ addLayer("u2", {
     baseAmount() { return player.r.points },  // A function to return the current amount of baseResource.
 	effect() { let eff = new Decimal(0.645).mul(player.u2.points)
 			   return eff },
-	effectDescription() { return hasUpgrade("u2", 15) ? "lowering wrong upgrade's exponent by -" + format(this.effect()) : "" },
+	effectDescription() { return hasUpgrade("u2", 14) ? "lowering wrong upgrade's exponent by -" + format(this.effect()) : "" },
 
-    requires() { let cost = new Decimal(5).pow((player.l.unlocked&&player.b.unlocked&&!player.u2.unlocked)?81:(player.l.unlocked&&!player.u2.unlocked || player.b.unlocked&&!player.u2.unlocked)?9:1)
+    requires() { let cost = new Decimal(5).pow((player.l.unlocked&&!player.u2.unlocked)?5.3:1)
 	             if(player.u2.points >= 3) cost = new Decimal(Infinity)
 				 return cost },             // The amount of the base needed to  gain 1 of the prestige currency.
                                             // Also the amount required to unlock the layer.
@@ -450,14 +553,14 @@ addLayer("u2", {
 			unlocked() { return hasUpgrade("u2", 12) }
 		},
 		14: {
-			title: "Upgrade = Save File.<br/>What.",
-			description: "Keeps wrong and right upgrade's first upgrades on 3rd row resets.",
+			title: "UPGRADE = PROFIT",
+			description: "Unique upgrades has effect and you can buy max 1st and 2nd row upgrades.",
 			cost: new Decimal(2),
 			unlocked() { return hasUpgrade("u2", 13) }
 		},
 		15: {
-			title: "UPGRADE = PROFIT",
-			description: "Unique upgrades has effect and you can buy max 1st and 2nd row upgrades.",
+			title: "Upgrade = Save File.<br/>What.",
+			description: "Keeps wrong and right upgrade's first upgrades on 3rd row resets.",
 			cost: new Decimal(2),
 			unlocked() { return hasUpgrade("u2", 14) }
 		},
@@ -471,13 +574,37 @@ addLayer("u2", {
 			title: "Upgrade = Milestone... Part 2",
 			description: "You keep bought upgrade's second upgrade on 2nd and 3rd row resets.",
 			cost: new Decimal(3),
-			unlocked() { return hasUpgrade("u2", 15) }
+			unlocked() { return hasUpgrade("u2", 21) }
 		},
 		23: {
 			title: "Upgrade = resetsNothing",
 			description: "2nd and 3rd row resets no longer resets bought upgrade layer.",
 			cost: new Decimal(3),
-			unlocked() { return hasUpgrade("u2", 15) }
+			unlocked() { return hasUpgrade("u2", 22) }
 		}
 	}
+})
+
+addLayer("a", {
+        startData() { return {
+            unlocked: true,
+			points: new Decimal(0)
+        }},
+        color: "yellow",
+        row: "side",
+		resource: "achievement upgrade",
+        layerShown() {return hasUpgrade("b", 42)}, 
+        tooltip() { // Optional, tooltip displays when the layer is locked
+            return ("Achievement")
+        },
+        upgrades: {
+            rows: 1,
+            cols: 1,
+			11: {
+                title: "YOU THOUGHT THERE REALLY WOULD BE AN ACHIEVEMENT...",
+                description: "Get 1 achievement upgrade.",
+				cost: new Decimal(0),
+				onPurchase() { player.a.points = new Decimal(1) }
+            }
+		}
 })
